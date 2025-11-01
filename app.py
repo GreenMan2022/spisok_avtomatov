5d1883458691ab39296822e1edfc3edba8bc0e82
GreenMan2022 <96194872+GreenMan2022@users.noreply.github.com>
2025-11-01 12:12:43 +0500

    Update app.py

--- app.py
+++ app.py
@@ -162,4 +162,5 @@ def export_spare_parts():
 
 if __name__ == '__main__':
     init_db()
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port, debug=False)
